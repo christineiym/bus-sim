84bcf91e52f5f9554b84c4fa968f0e4a5619d71b
christineiym <mendoza@cmu.edu>
2025-01-06 12:22:46 -0500

    prepare for deploy

--- flask_single_route.py
+++ flask_single_route.py
@@ -219,5 +219,6 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host='0.0.0.0', port=port)
